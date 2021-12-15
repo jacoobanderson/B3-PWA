e059ddeb8def9e9f32ded7e4bad21cc1d66f7b79
jacoobanderson <j00cwa@gmail.com>
2021-12-15 15:46:47 +0100

    Style prototype window

--- src/js/components/draggable-window/draggable-window.js
+++ src/js/components/draggable-window/draggable-window.js
@@ -9,19 +9,67 @@ template.innerHTML = `
             margin: 0;
             width: 400px;
             height: 300px;
-            border: solid 2px white;
+            border: solid 1px white;
+            border-radius: 7px;
         }
+
+        .button {
+            width: 15px;
+            height: 15px;
+            border-radius: 50%;
+            border: none;
+            margin: 2px;
+        }
+
+        .button:hover {
+            background-color: white;
+        }
+
+        .menu {
+            background-color: grey;
+            align-items: center;
+        }
+
+        .exit {
+            background-color: red;
+        }
+        .yellow {
+            background-color: yellow;
+        }
+        .green {
+            background-color: green;
+        }
+
     </style>
+    <div class="menu">
+        <button class="button exit"></button>
+        <button class="button yellow"></button>
+        <button class="button green"></button>
+    </div>
+    <div class="appcontainer">
+        <slot name="app"></slot>
+    </div>
 `
 
 customElements.define('draggable-window',
 
     class extends HTMLElement {
 
+        #exit
+
         constructor() {
             super()
             this.attachShadow({ mode: 'open' })
                 .appendChild(template.content.cloneNode(true))
+
+            this.#exit = this.shadowRoot.querySelector('.exit')
         }
 
+        connectedCallback() {
+            this.#exit.addEventListener('click', (event) => this.#exitApp(event))
+        }
+
+        #exitApp () {
+            this.style.display = 'none'
+        }
     })
