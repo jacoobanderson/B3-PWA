4dfbf6cd06886bdef647644005a96a4c6ac0b28f
jacoobanderson <j00cwa@gmail.com>
2022-01-08 02:41:54 +0100

    add chat styling

--- src/js/components/chat-app/chat-app.js
+++ src/js/components/chat-app/chat-app.js
@@ -4,28 +4,43 @@ template.innerHTML = `
     <style>
         :host {
             display: block;
-            height: 400px;
+            height: 500px;
             width: 650px;
             background-color: rgb(44, 44, 46)
         }
-        
+
         .chatapp {
             height: 100%;
             width: 100%;
-
+            display: grid;
+            grid-template-rows: 1fr 1fr 1fr 1fr 1fr;
         }
 
         .chatmessages {
             background-color: white;
-            width: 100%;
-            height: 70%
+            width: 95%;
+            height: 95%;
+            color: white;
+            grid-row: 1/5;
+            justify-self: center;
+            align-self: center;
+            background-color: rgb(58, 58, 58);
+            border-radius: 8px;
         }
 
         .sender {
             display: block;
-            width: 100%;
-            height: 30%;
+            width: 95%;
+            height: 90%;
             resize: none;
+            grid-row: 5/6;
+            justify-self: center;
+            align-self: center;
+            padding: 4px;
+            margin: 0;
+            margin-bottom: 6px;
+            outline: none;
+            border-radius: 8px;
         }
     </style>
     <div class="chatapp">
